40605f789847ba2f5107605b924b34bae1b12f77
yanagidakouya <yanagidakouya@gmail.com>
2021-02-13 10:55:49 +0900

    json fix

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+// fontawesome
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
-// import { faGlobe,faBlog,faBirthdayCake,faImage } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-// import { faTwitter,faGithub,faHtml5,faCss3Alt,faJsSquare } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 library.add(fas, fab)
-// library.add(faGlobe,faBlog,faBirthdayCake,faImage,faTwitter,faGithub,faHtml5,faCss3Alt,faJsSquare)
-
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+// vue-particles
+import VueParticles from 'vue-particles'
+Vue.use(VueParticles)
 // sassのimport
 require('@/assets/sass/app.scss')
 
-Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 
 new Vue({
